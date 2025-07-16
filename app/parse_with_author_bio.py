b1e94d4f78c0bdee6834e567832c2710e1a97ac3
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-07-16 18:03:08 +0200

    Implement parser to collect author's biography for each quote

--- app/parse_with_author_bio.py
+++ app/parse_with_author_bio.py
@@ -13,13 +13,24 @@ session = requests.Session()
 
 
 @dataclass
+class Author:
+    name: str
+    birth_date: str
+    bio: str
+
+
+AUTHOR_FIELDS = [field.name for field in fields(Author)]
+
+
+@dataclass
 class Quote:
     text: str
     author: str
     tags: list[str]
+    author_data: Author
 
 
-QUOTE_FIELDS = [field.name for field in fields(Quote)]
+QUOTE_FIELDS_WITHOUT_AUTHOR_DATA = [field.name for field in fields(Quote)][:-1]
 
 logging.basicConfig(
     level=logging.INFO,
@@ -31,9 +42,36 @@ logging.basicConfig(
 )
 
 
+def get_page_soup(url: str) -> BeautifulSoup:
+    response = session.get(url)
+    return BeautifulSoup(response.text, "html.parser")
+
+
+def parse_author_bio_from_page(page_soup: BeautifulSoup) -> Author:
+    name = page_soup.select_one("h3.author-title").text
+    birth_date = page_soup.select_one("span.author-born-date").text
+    bio = page_soup.select_one("div.author-description").text.lstrip()
+
+    return Author(name=name, birth_date=birth_date, bio=bio)
+
+
+author_cache: dict[str, Author] = {}
+
+
+def get_author(url: str) -> Author:
+    if url in author_cache:
+        logging.debug(f"Author from cache: {url}")
+        return author_cache[url]
+
+    logging.info(f"Fetching author bio: {url}")
+    soup = get_page_soup(url)
+    author = parse_author_bio_from_page(soup)
+    author_cache[url] = author
+    return author
+
+
 def parse_one_quote(quote: Tag) -> Quote:
     text = quote.select_one("span.text").text
-    author = quote.select_one("small.author").text
 
     tags_container = quote.select_one("div.tags")
     tags = [
@@ -41,12 +79,11 @@ def parse_one_quote(quote: Tag) -> Quote:
         for tag in tags_container.find_all("a", class_="tag")
     ]
 
-    return Quote(text=text, author=author, tags=tags)
+    author_bio_url = quote.find("a", string="(about)").get("href")
+    author_bio_url = urljoin(BASE_URL, author_bio_url)
+    author = get_author(author_bio_url)
 
-
-def get_page_soup(url: str) -> BeautifulSoup:
-    response = session.get(url)
-    return BeautifulSoup(response.text, "html.parser")
+    return Quote(text=text, author=author.name, tags=tags, author_data=author)
 
 
 def get_next_page_soup(page_soup: BeautifulSoup) -> BeautifulSoup | None:
@@ -83,14 +120,27 @@ def get_all_pages_quotes() -> list[Quote]:
 def write_quotes_to_csv(quotes: list[Quote], output_csv_path: str) -> None:
     with open(output_csv_path, "w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(QUOTE_FIELDS)
-        writer.writerows([astuple(quote) for quote in quotes])
+        writer.writerow(QUOTE_FIELDS_WITHOUT_AUTHOR_DATA)
+        # Get tuple without author_data ("text", "author", "tags")
+        writer.writerows([astuple(quote)[:-1] for quote in quotes])
+    logging.info("Quotes written to csv...!")
+
+
+def write_author_to_csvs(quotes: list[Quote]) -> None:
+    authors = []
+    for quote in quotes:
+        authors.append(astuple(quote.author_data))
+    with open("authors_bio.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(AUTHOR_FIELDS)
+        writer.writerows(authors)
+    logging.info("Authors written to csv...!")
 
 
 def main(output_csv_path: str) -> None:
     quotes_list = get_all_pages_quotes()
     write_quotes_to_csv(quotes_list, output_csv_path)
-    logging.info("Parsed all quotes...!")
+    write_author_to_csvs(quotes_list)
 
 
 if __name__ == "__main__":
